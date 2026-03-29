8fc894d4ce04f4c258cceb41b4f8d58a6c1c893b
Ron Rieck <ron.rieck@gmx.de>
2026-03-29 16:56:36 +0200

    outgoing exception handling and accounts

---
+++ src/main/java/com/rrieck/taskmanagementbackend/accountMemeber/service/EditMemberRoleService.java
@@ -0,0 +1,14 @@
+package com.rrieck.taskmanagementbackend.accountMemeber.service;
+
+import com.rrieck.taskmanagementbackend.accountMemeber.repository.AccountMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EditMemberRoleService {
+	private final AccountMemberRepository accountMemberRepository;
+
+	public void EditMemberRoleService() {
+	}
+}
